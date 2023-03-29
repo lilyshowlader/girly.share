fcf8306a8cbbb9947b03c669dfd3b3ee21fb523e
lilyshowlader <lilyshowlader@gmail.com>
2023-03-29 18:30:11 -0400

    add routes for users and products, create pages

---
+++ src/user/components/UserItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './UserItem.css'
+
+function UserItem (props) {
+
+
+}
+
+export default UsersItem
---
+++ src/user/pages/Users.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <>
+    <h2> Users works! </h2>
+    </>
+  )
+}
+
+export default Users
