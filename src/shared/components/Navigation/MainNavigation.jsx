a55827379593fabaf0e8279f53068b91909f7002
lilyshowlader <lilyshowlader@gmail.com>
2023-04-17 19:50:31 -0400

    add side drawer

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -3,18 +3,23 @@ import './MainNavigation.css'
 import MainHeader from './MainHeader'
 import { Link } from 'react-router-dom'
 import NavLinks from './NavLinks'
-
+import SideDrawer from './SideDrawer'
 export default function MainNavigation(props) {
 	return (
 		<>
+		<SideDrawer>
+			<nav className='main-navigation__drawer-nav'>
+				<NavLinks/>
+			</nav>
+		</SideDrawer>
 		<MainHeader>
 			<button className='main-navigation__menu-btn'>
 				<span />
 				<span />
 				<span />
 			</button>
-			<h1 className='main-navigation__title'><Link to='/'>my products</Link></h1>
-			<nav>
+			<h1 className='main-navigation__title'><Link to='/'>girlyhealth.share</Link></h1>
+			<nav className='main-navigation__header-nav'>
 				<NavLinks />
 			</nav>
 		</MainHeader>
