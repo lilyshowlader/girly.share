e965602f142a0b684eb6338cd267940a96e84867
lilyshowlader <lilyshowlader@gmail.com>
2023-04-25 18:49:02 -0400

    add validators.js file

--- src/products/pages/NewProduct.jsx
+++ src/products/pages/NewProduct.jsx
@@ -4,7 +4,9 @@ import './NewProduct.css'
 function NewProduct() {
   return(
     <form className='place-form'>
-      <Input element='input' type='text' label='Title'/>
+      <Input element='input' type='text' label='Name' validators={[]} errorText='please enter a valid title' />
+      <Input element='textarea' type='text' label='Description' validators={[]} errorText='please enter a valid title' />
+      <Input element='input' type='text' label='Link' validators={[]} errorText='please enter a valid title' />
     </form>
   )
 }
