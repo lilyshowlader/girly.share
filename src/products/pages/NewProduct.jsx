5769300add5125e8753cd0fbe192750ccb03a7cb
lilyshowlader <lilyshowlader@gmail.com>
2023-05-02 13:18:50 -0400

    add description and link input handler

--- src/products/pages/NewProduct.jsx
+++ src/products/pages/NewProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Input from '../../shared/components/FormElements/Input'
 import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import './NewProduct.css'
@@ -6,20 +6,41 @@ import './NewProduct.css'
 
 
 function NewProduct() {
-  const titleInputHandler = (id, value, isValid) => {
-
-  }
+  const titleInputHandler = useCallback((id, value, isValid) => {}, [])
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, [])
+  const linkInputHandler = useCallback((id, value, isValid) => {}, [])
 
 
   return (
     <form className='place-form'>
       <Input 
+      id='title'
       element='input' 
       type='text' 
-      label='Name' 
+      label='name' 
       validators={[VALIDATOR_REQUIRE()]} 
       errorText='please enter a valid title' 
       onInput={titleInputHandler}
+      />
+
+      <Input 
+      id='description'
+      element='textarea' 
+      type='text' 
+      label='description' 
+      validators={[VALIDATOR_REQUIRE()]} 
+      errorText='please enter a description!' 
+      onInput={descriptionInputHandler}
+      />
+
+      <Input 
+      id='link'
+      element='input' 
+      type='text' 
+      label='link' 
+      validators={[VALIDATOR_REQUIRE()]} 
+      errorText='please enter a valid title' 
+      onInput={linkInputHandler}
       />
     </form>
   )
