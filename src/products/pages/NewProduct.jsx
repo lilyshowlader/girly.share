552eabd270a3d121c977e5fc81d855113a7d8084
lilyshowlader <lilyshowlader@gmail.com>
2023-05-02 13:38:54 -0400

    add button to new product

--- src/products/pages/NewProduct.jsx
+++ src/products/pages/NewProduct.jsx
@@ -1,14 +1,56 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useReducer } from 'react'
 import Input from '../../shared/components/FormElements/Input'
+import Button from'../../shared/components/FormElements/Button'
 import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import './NewProduct.css'
 
-
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        input: {
+          ...state.inputs,
+          [action.inputId] : { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      }
+    default:
+      return state 
+  }
+}
 
 function NewProduct() {
-  const titleInputHandler = useCallback((id, value, isValid) => {}, [])
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, [])
-  const linkInputHandler = useCallback((id, value, isValid) => {}, [])
+  // initial states
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: 'false'
+      },
+      description: {
+        value: '',
+        isValid: 'false'
+      },
+      link: {
+        value: '',
+        isValid: 'false'
+      },
+    },
+    isValid: false
+  })
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({ type: "INPUT_CHANGE", value: value, isValid: isValid, inputId: id})
+  }, [])
 
 
   return (
@@ -20,7 +62,7 @@ function NewProduct() {
       label='name' 
       validators={[VALIDATOR_REQUIRE()]} 
       errorText='please enter a valid title' 
-      onInput={titleInputHandler}
+      onInput={inputHandler}
       />
 
       <Input 
@@ -30,7 +72,7 @@ function NewProduct() {
       label='description' 
       validators={[VALIDATOR_REQUIRE()]} 
       errorText='please enter a description!' 
-      onInput={descriptionInputHandler}
+      onInput={inputHandler}
       />
 
       <Input 
@@ -39,9 +81,12 @@ function NewProduct() {
       type='text' 
       label='link' 
       validators={[VALIDATOR_REQUIRE()]} 
-      errorText='please enter a valid title' 
-      onInput={linkInputHandler}
+      errorText='please enter a link' 
+      onInput={inputHandler}
       />
+      <Button type='submit' disabled={!formState.isValid}>
+        add product
+      </Button>
     </form>
   )
 }
